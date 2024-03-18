d3bf3d776f3d37612d578d2c131c3adea51a6e7b
Tarraneh Eftekhari <etarraneh@gmail.com>
2024-03-18 10:53:12 -0500

    Update magnitude

--- src/components/main.js
+++ src/components/main.js
@@ -331,7 +331,7 @@ const Main = () => {
                           <td>{rowData["SFR ($\rm M_{\\odot}$\\yr)"]}</td>
                           <td>{rowData["$Z_{\rm gas}$ ($Z_{\\odot}$)"]}</td>
                           <td>{rowData["$t_{m}$ (Gyr)"]}</td>
-                          <td>{rowData["$m_r$."]}</td>
+                          <td>{rowData["$m_r$."].toFixed(2)}</td>
                           <td>{rowData.redshift}</td>
                         </tr>
                       );
