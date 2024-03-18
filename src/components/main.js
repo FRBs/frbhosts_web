ec874d1757acab00b6d8e1288022a9761e17c1fe
Tarraneh Eftekhari <etarraneh@gmail.com>
2024-03-18 14:13:38 -0500

    fixing m_r

--- src/components/main.js
+++ src/components/main.js
@@ -293,7 +293,7 @@ const Main = () => {
                         String(e["SFR ($\rm M_{\\odot}$\\yr)"])?.includes(String(search)) ||
                         String(e["$Z_{\rm gas}$ ($Z_{\\odot}$)"])?.includes(String(search)) ||
                         String(e["$t_{m}$ (Gyr)"])?.includes(String(search)) ||
-                        String(e["$m_r$."].toFixed(2))?.includes(String(search)) ||
+                        String(e["$m_r$."])?.includes(String(search)) ||
                         String(e.Redshift.includes(String(search)))
                     )
                     .map((ini, i) => {
@@ -331,7 +331,7 @@ const Main = () => {
                           <td>{rowData["SFR ($\rm M_{\\odot}$\\yr)"]}</td>
                           <td>{rowData["$Z_{\rm gas}$ ($Z_{\\odot}$)"]}</td>
                           <td>{rowData["$t_{m}$ (Gyr)"]}</td>
-                          <td>{rowData["$m_r$."]}</td>
+                          <td>{test}</td>
                           <td>{rowData.redshift}</td>
                         </tr>
                       );
